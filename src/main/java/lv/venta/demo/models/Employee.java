65cf05c3a02fe98d455b5ed222c35da3841d0111
BadMoodDuck <2dwarriorjonny@gmail.com>
2022-06-25 18:02:19 +0300

    Added validations and constructors

--- src/main/java/lv/venta/demo/models/Employee.java
+++ src/main/java/lv/venta/demo/models/Employee.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -34,6 +36,8 @@ public class Employee {
 	
 	//TODO Validacijas
 	@Column(name="Name")
+	@Size(min=1, max=40)
+	@Pattern(regexp="[a-zA-Z]+(.|\\s)*")
 	private String name;
 
 	//TODO parejie mainigie
@@ -44,5 +48,12 @@ public class Employee {
 	@OneToMany(mappedBy = "employee")
 	@ToString.Exclude
 	private Collection<EmployeeCourse> emCourse;
+	
+	public Employee(String name,Department department,Collection<EmployeeCourse> emCourse) {
+		this.name=name;
+		this.department=department;
+		this.emCourse=emCourse;
+		
+	}
 		
 }
