7f19480d9a048a07bdb6d3d385218df9abf77615
evapank <s21pankeva@venta.lv>
2022-10-21 09:48:19 +0300

    MyUserDetails and MyUserAuthority

---
+++ src/main/java/lv/venta/demo/config/MyUserDetails.java
@@ -0,0 +1,70 @@
+package lv.venta.demo.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lv.venta.demo.models.MyUser;
+import lv.venta.demo.models.MyUserAuthority;
+
+public class MyUserDetails implements UserDetails{
+
+	private MyUser user;
+	
+	public MyUserDetails(MyUser user) {
+		this.user = user;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<GrantedAuthority> allAuthoritiesForUser = new ArrayList<>();
+
+		Iterator<MyUserAuthority> iter = user.getAllAuthorities().iterator();
+		while (iter.hasNext()) {
+			allAuthoritiesForUser.add(new SimpleGrantedAuthority(iter.next().getTitle()));
+		}
+
+		return allAuthoritiesForUser;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
--- src/main/java/lv/venta/demo/models/MyUserAuthority.java
+++ src/main/java/lv/venta/demo/models/MyUserAuthority.java
@@ -1,5 +1,49 @@
 package lv.venta.demo.models;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "myuserauthority_table")
+@Entity
 public class MyUserAuthority {
+
+		@Column(name = "IdA")
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		@Setter(value = AccessLevel.NONE)
+		private int idA;
+
+		@Column(name = "Title")
+		//@Pattern(regexp = "[ROLE_][A-Z]+")
+		private String title;
+
+		@ManyToMany
+		@JoinTable(name = "users_authorities", joinColumns = @JoinColumn(name = "IdA"), inverseJoinColumns = @JoinColumn(name = "IdU"))
+		private Collection<MyUser> myUsers = new ArrayList<>();
+
+		public void addUser(MyUser user) {
+			myUsers.add(user);
+		}
+		public void deleteUser(MyUser user) {
+			myUsers.remove(user);
+		}
 
 }
