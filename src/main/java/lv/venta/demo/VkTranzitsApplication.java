f8d1c30120c6cc022a3cf1252e5fd51e00e436d6
MANSBlv <marisansbergs@gmail.com>
2022-11-04 11:53:47 +0200

    optimized for my branch

--- src/main/java/lv/venta/demo/VkTranzitsApplication.java
+++ src/main/java/lv/venta/demo/VkTranzitsApplication.java
@@ -60,101 +60,101 @@ public class VkTranzitsApplication {
 			public void run(String... args) throws Exception {
 				
 
-				Company com = new Company("Jhons");
-				Company com1 = new Company("Does");
-				Company com2 = new Company("Fixer");
-				companyRepo.save(com);
-				companyRepo.save(com1);
-				companyRepo.save(com2);
-				
-
-				Position pos = new Position("Plumber","They do plumbin");
-				Position pos1 = new Position("Electrician","They do electricing");
-				Position pos2 = new Position("Pekachus","They do pika pi");
-				positionRepo.save(pos);
-				positionRepo.save(pos1);
-				positionRepo.save(pos2);
-				
-				
-				Department dep = new Department(com,"Plumbas");
-				Department dep1 = new Department(com1,"Electricans");
-				Department dep2 = new Department(com2,"Piekachus");
-				departmentRepo.save(dep);
-				departmentRepo.save(dep1);
-				departmentRepo.save(dep2);
-				
-				Employee emp = new Employee("Jhon","Silver",22134570,"abce@gmail.com",dep,pos);
-				Employee emp1 = new Employee("Jevgenijs","Saimnieks",22564540,"sert@gmail.com",dep1,pos1);
-				Employee emp2 = new Employee("Janis","Sudrabs",22626561,"asdf@gmail.com",dep1,pos1);
-				Employee emp3 = new Employee("Kristaps","Slieka",22134560,"rtyh@gmail.com",dep2,pos);
-				Employee emp4 = new Employee("Maris","Smiekls",22555660,"zxcv@gmail.com",dep2,pos2);
-				Employee emp5 = new Employee("Lone","Man",22512310,"abece@gmail.com",dep,pos2);
-				Employee emp7 = new Employee("David","Martinez",22525250,"davidmartinez@arosaka.com",dep1,pos);
-				employeeRepo.save(emp);
-				employeeRepo.save(emp1);
-				employeeRepo.save(emp2);
-				employeeRepo.save(emp3);
-				employeeRepo.save(emp4);
-				employeeRepo.save(emp5);
-				employeeRepo.save(emp7);
-				
-				
-				CourseType cty = new CourseType("Ugunsdrosiba", false, "Viss vajadzigais par ugunsdrosiba");
-				CourseType cty1 = new CourseType("Darba drosiba", true, "Viss vajadzigais par darba drosiba");
-				courseTypeRepo.save(cty);
-				courseTypeRepo.save(cty1);
-				
-				Course co = new Course(cty,"1.Ugunsdrosibas ievads","Viss galvenais par uguni");
-				Course co1 = new Course(cty1,"1.Darba drosibas ievads","Viss galvenais par darbu");
-				courseRepo.save(co);
-				courseRepo.save(co1);
-				
-				
-				dep.addNewCourse(co);
-				dep1.addNewCourse(co);
-				dep2.addNewCourse(co1);
-				departmentRepo.save(dep);
-				departmentRepo.save(dep1);
-				departmentRepo.save(dep2);
-
-				
-				EmployeeCourse emc = new EmployeeCourse(emp7, co1, 4);
-				EmployeeCourse emc1 = new EmployeeCourse(emp2, co, 10);
-				employeeCourseRepo.save(emc);
-				employeeCourseRepo.save(emc1);
-				
-				Implementer imp = new Implementer("Viktors Vikis");
-				Implementer imp1 = new Implementer("Janis Vikis");
-				implementerRepo.save(imp);
-				implementerRepo.save(imp1);
-				
-				CourseCalendar cal = new CourseCalendar(2023, new Date(System.currentTimeMillis()),new Date(System.currentTimeMillis()));
-				courseCalendarRepo.save(cal);
-				
-				
-				CourseImplementer cim = new CourseImplementer(imp,  cal, "Not important");
-				CourseImplementer cim1 = new CourseImplementer(imp1, cal, "Damn this hard");
-				courseImplementerRepo.save(cim);
-				courseImplementerRepo.save(cim1);
-				
+//				Company com = new Company("Jhons");
+//				Company com1 = new Company("Does");
+//				Company com2 = new Company("Fixer");
+//				companyRepo.save(com);
+//				companyRepo.save(com1);
+//				companyRepo.save(com2);
+//				
+//
+//				Position pos = new Position("Plumber","They do plumbin");
+//				Position pos1 = new Position("Electrician","They do electricing");
+//				Position pos2 = new Position("Pekachus","They do pika pi");
+//				positionRepo.save(pos);
+//				positionRepo.save(pos1);
+//				positionRepo.save(pos2);
+//				
+//				
+//				Department dep = new Department(com,"Plumbas");
+//				Department dep1 = new Department(com1,"Electricans");
+//				Department dep2 = new Department(com2,"Piekachus");
+//				departmentRepo.save(dep);
+//				departmentRepo.save(dep1);
+//				departmentRepo.save(dep2);
+//				
+//				Employee emp = new Employee("Jhon","Silver",22134570,"abce@gmail.com",dep,pos);
+//				Employee emp1 = new Employee("Jevgenijs","Saimnieks",22564540,"sert@gmail.com",dep1,pos1);
+//				Employee emp2 = new Employee("Janis","Sudrabs",22626561,"asdf@gmail.com",dep1,pos1);
+//				Employee emp3 = new Employee("Kristaps","Slieka",22134560,"rtyh@gmail.com",dep2,pos);
+//				Employee emp4 = new Employee("Maris","Smiekls",22555660,"zxcv@gmail.com",dep2,pos2);
+//				Employee emp5 = new Employee("Lone","Man",22512310,"abece@gmail.com",dep,pos2);
+//				Employee emp7 = new Employee("David","Martinez",22525250,"davidmartinez@arosaka.com",dep1,pos);
+//				employeeRepo.save(emp);
+//				employeeRepo.save(emp1);
+//				employeeRepo.save(emp2);
+//				employeeRepo.save(emp3);
+//				employeeRepo.save(emp4);
+//				employeeRepo.save(emp5);
+//				employeeRepo.save(emp7);
+//				
+//				
+//				CourseType cty = new CourseType("Ugunsdrosiba", false, "Viss vajadzigais par ugunsdrosiba");
+//				CourseType cty1 = new CourseType("Darba drosiba", true, "Viss vajadzigais par darba drosiba");
+//				courseTypeRepo.save(cty);
+//				courseTypeRepo.save(cty1);
+//				
+//				Course co = new Course(cty,"1.Ugunsdrosibas ievads","Viss galvenais par uguni");
+//				Course co1 = new Course(cty1,"1.Darba drosibas ievads","Viss galvenais par darbu");
+//				courseRepo.save(co);
+//				courseRepo.save(co1);
+//				
+//				
+//				dep.addNewCourse(co);
+//				dep1.addNewCourse(co);
+//				dep2.addNewCourse(co1);
+//				departmentRepo.save(dep);
+//				departmentRepo.save(dep1);
+//				departmentRepo.save(dep2);
+//
+//				
+//				EmployeeCourse emc = new EmployeeCourse(emp7, co1, 4);
+//				EmployeeCourse emc1 = new EmployeeCourse(emp2, co, 10);
+//				employeeCourseRepo.save(emc);
+//				employeeCourseRepo.save(emc1);
+//				
+//				Implementer imp = new Implementer("Viktors Vikis");
+//				Implementer imp1 = new Implementer("Janis Vikis");
+//				implementerRepo.save(imp);
+//				implementerRepo.save(imp1);
+//				
+//				CourseCalendar cal = new CourseCalendar(2023, new Date(System.currentTimeMillis()),new Date(System.currentTimeMillis()));
+//				courseCalendarRepo.save(cal);
+//				
+//				
+//				CourseImplementer cim = new CourseImplementer(imp,  cal, "Not important");
+//				CourseImplementer cim1 = new CourseImplementer(imp1, cal, "Damn this hard");
+//				courseImplementerRepo.save(cim);
+//				courseImplementerRepo.save(cim1);
+//				
 				MyUserAuthority auth1 = new MyUserAuthority("ROLE_ADMIN");
 				MyUserAuthority auth2 = new MyUserAuthority("ROLE_IMPLEMENTER");
 				MyUserAuthority auth3 = new MyUserAuthority("ROLE_EMPLOYEE");
 				MyUserAuthority auth4 = new MyUserAuthority("ROLE_GUEST");
 				
-				authorityRepo.save(auth1);
-				authorityRepo.save(auth2);
-				authorityRepo.save(auth3);
+//				authorityRepo.save(auth1);
+//				authorityRepo.save(auth2);
+//				authorityRepo.save(auth3);
+//				authorityRepo.save(auth4);
+////				
+				MyUser user1 = new MyUser("guestss", encoder.encode("guestss"));
+				
+				userRepo.save(user1);
+				
+				user1.addAuthority(auth4);
+				userRepo.save(user1);
+				auth4.addUser(user1);
 				authorityRepo.save(auth4);
-				
-				MyUser user1 = new MyUser("admins", encoder.encode("admins"));
-				
-				userRepo.save(user1);
-				
-				user1.addAuthority(auth1);
-				userRepo.save(user1);
-				auth1.addUser(user1);
-				authorityRepo.save(auth1);
 				
 			}
 		};
