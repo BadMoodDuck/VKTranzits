ea5d6d5174ac033d1184539ed8e6c07d2722ee36
Māris Ansbergs <98128317+MANSBlv@users.noreply.github.com>
2022-12-23 01:53:25 +0200

    Calendar frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, {useEffect, useState} from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import axios from "axios";
 function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [events, setEvents] = useState([]);
+
+
+    useEffect(() => {
+     
+        axios.get("http://localhost:8080/api/calendar/dates")
+            .then(response => {
+                console.log("Received dates:", response.data);
+                const events = response.data.map(date => ({
+                    start: date.startDate,
+                    end: date.endDate,
+                    title: date.courseName
+                }));
+                setEvents(events);
+            })
+            .catch(error => {
+                console.error("Error fetching events:", error);
+            });
+    }, []);
+
+   
+
+  const localizer = momentLocalizer(moment);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Calendar
+          localizer={localizer}
+          events={events}
+          defaultDate={moment().toDate()}
+          startAccessor="start"
+          endAccessor="end"
+          style={{height:600}}
+      />
   );
 }
 
+
+
 export default App;
+
