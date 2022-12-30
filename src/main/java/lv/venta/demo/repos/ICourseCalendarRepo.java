ccfa329b86a4d6bceee837ca645be4fd9342fe9e
Māris Ansbergs <98128317+MANSBlv@users.noreply.github.com>
2022-12-30 17:26:11 +0200

    removed unused import

--- src/main/java/lv/venta/demo/repos/ICourseCalendarRepo.java
+++ src/main/java/lv/venta/demo/repos/ICourseCalendarRepo.java
@@ -1,11 +1,6 @@
 package lv.venta.demo.repos;
 
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
 import lv.venta.demo.models.CourseCalendar;
 
 public interface ICourseCalendarRepo extends JpaRepository<CourseCalendar, Integer> {
